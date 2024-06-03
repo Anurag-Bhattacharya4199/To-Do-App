099dd4a2c8c8e2e914b1d15c5ca5374b346ba38a
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2024-06-03 13:38:25 -0400

    Day 15 - Standard Modules

--- modules/functions.py
+++ modules/functions.py
@@ -1,11 +1,13 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
   """ Read a text file and return a list of todo items"""
   with open(filepath, 'r') as file_local: 
     todos_local = file_local.readlines()
   return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
   """ Write the todo items into a text file"""
   with open(filepath, 'w') as file:
       file.writelines(todos_arg)
